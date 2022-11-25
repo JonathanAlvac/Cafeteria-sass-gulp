230f4879116c2df9cf4f51385949525205d7b97e
Jonathan Alvarez <jonathancodeem@gmail.com>
2022-11-25 14:01:03 -0500

    Agregando librerias gulp- automatizacion imagenes

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,43 @@
 const { src, dest, watch, series, parallel } = require('gulp');
+
+//CSS y SASS
 const sass = require('gulp-sass')(require('sass'));
 const postcss = require('gulp-postcss');
 const autoprefixer = require('autoprefixer');
 
+//Imagenes
+const imagemin = require('gulp-imagemin');
+const webp = require('gulp-webp');
 
-function css(done) {
+
+function css() {
     //compilar sass
     //paso 1:Indentificar archivos, 2-compilar, 3-guardar
 
-    src('src/scss/app.scss')
+    return src('src/scss/app.scss')
         .pipe(sass())
-        .pipe( postcss([autoprefixer()]))
+        .pipe(postcss([autoprefixer()]))
         .pipe(dest('build/css'))
 
-    done();
+
 }
 
-function dev(){
-    watch('src/scss/**/*.scss', css)
+function imagenes() {
+    //1, identifica, 2-guardar
+    return src('src/img/**/*') //colocando return no es necesario el done
+        .pipe(imagemin({ optimizationLevel: 3 }))
+        .pipe(dest('build/img'));
+}
+
+function versionWebp(){
+    return src('src/img/**/*.{png,jpg}')
+        .pipe(webp())
+        .pipe(dest('build/img'))
+}
+
+function dev() {
+    watch('src/scss/**/*.scss', css);
+    watch('src/img/**/*', imagenes);
 }
 
 function tareaDefault() {
@@ -26,7 +46,10 @@ function tareaDefault() {
 
 exports.css = css;
 exports.dev = dev;
-exports.default = series( css, dev);
+exports.imagenes = imagenes;
+exports.versionWebp = versionWebp;
+exports.default = series(imagenes, versionWebp, css, dev);
+
 
 //Series : Se inicia una tarea, hasta que finaliza inicia la siguiente
 //parallel : Todas inician al mismo tiempo
